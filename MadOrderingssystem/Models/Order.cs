1371e8a367d08308f49676da3e445087ac25767d
Mikkel Hartmann Jensen <mikk4472@edu.easj.dk>
2020-12-02 11:54:53 +0100

    Lavet Order

---
+++ MadOrderingssystem/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Order
+{
+	public Order()
+	{
+		//Skal indeholde
+		//Guid id,(List) Products, Customer
+		List<Product> _products;
+		Customer _customer;
+		Guid _id;
+
+		public Order(Customer newCustomer, List<Product> newProducts, Guid newId)
+		{
+			//Guid id
+			_id = newId;
+
+			//Products
+			_products = newProducts;
+
+			//Customer
+			_customer = newCustomer;
+		}
+}
