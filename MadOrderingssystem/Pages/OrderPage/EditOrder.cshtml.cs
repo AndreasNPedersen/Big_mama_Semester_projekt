af5cf71cf9c72b9a99c6b508cad06243565851b6
Mikkel Hartmann Jensen <mikk4472@edu.easj.dk>
2020-12-09 13:09:09 +0100

    Order CRUD

--- MadOrderingssystem/Pages/OrderPage/EditOrder.cshtml.cs
+++ MadOrderingssystem/Pages/OrderPage/EditOrder.cshtml.cs
@@ -2,15 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MadOrderingssystem.Models;
+using MadOrderingssystem.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace MadOrderingssystem.Pages.OrderPage
 {
     public class EditOrderModel : PageModel
     {
-        public void OnGet()
+
+
+        public Customer CustomerSession { get; set; }
+        [BindProperty]
+        public Order Order { get; set; }
+
+
+
+        public void OnGet(string id)
         {
+            OrderHandler oH = new OrderHandler();
+            Order = oH.Get(id);
+            try
+            {
+                CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
+            }
+            catch (ArgumentNullException ex) { }
+
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            OrderHandler oH = new OrderHandler();
+            oH.Update(Order, Order.ID);
+
+            return RedirectToPage("OrderList");
+
         }
     }
 }
