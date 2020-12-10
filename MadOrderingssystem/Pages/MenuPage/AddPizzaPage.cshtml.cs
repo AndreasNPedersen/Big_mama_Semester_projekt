10303182d388e1f797b7aaa728fa08aab3c23064
AlexTriumf <TriumfAlex@gmail.com>
2020-12-10 11:50:17 +0100

    updated

--- MadOrderingssystem/Pages/MenuPage/AddPizzaPage.cshtml.cs
+++ MadOrderingssystem/Pages/MenuPage/AddPizzaPage.cshtml.cs
@@ -5,39 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MadOrderingssystem.Models;
-using MadOrderingssystem.Services;
 using Newtonsoft.Json;
+using MadOrderingssystem.Services;
 using Microsoft.AspNetCore.Http;
 
 namespace MadOrderingssystem.Pages.MenuPage
 {
-    public class AddPageModel : PageModel
+    public class AddPizzaPageModel : PageModel
     {
+       
         public Customer CustomerSession { get; set; }
-        private string id { get; set; }
-        public string exeptionMSG { get; set; }
+
         [BindProperty]
-        public Product Product { get; set; }
+        public Pizza Pizza { get; set; }
+
         public void OnGet()
         {
-            //try
-            //{
-            //    CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
-            //}
-            //catch (ArgumentNullException ex) { exeptionMSG = "Du er ikke logget ind"; }
-
+            try
+            {
+                CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
+            }
+            catch (ArgumentNullException ex) { }
         }
-
-
 
         public IActionResult OnPost()
         {
-
             if (ModelState.IsValid)
             {
 
-                ProductHandler ProductHandler = new ProductHandler();
-                ProductHandler.Create(Product);
+
+                ProductHandler pH = new ProductHandler();
+                pH.Create(Pizza);
 
                 return RedirectToPage("/Index");
 
