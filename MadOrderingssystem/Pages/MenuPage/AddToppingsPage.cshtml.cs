da966b189202654f1fc29aebaf95b9ad55517813
Mikkel Hartmann Jensen <mikk4472@edu.easj.dk>
2020-12-10 12:02:21 +0100

    AddToppingsPage cs og html

---
+++ MadOrderingssystem/Pages/MenuPage/AddToppingsPage.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MadOrderingssystem.Models;
+using MadOrderingssystem.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
+
+namespace MadOrderingssystem.Pages.MenuPage
+{
+    public class AddToppingsPageModel : PageModel
+    {
+        public Customer CustomerSession { get; set; }
+
+        [BindProperty]
+        public Toppings Toppings { get; set; }
+
+        public void OnGet()
+        {
+            try
+            {
+                CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
+            }
+            catch (ArgumentNullException ex) { }
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+
+
+                ProductHandler pH = new ProductHandler();
+                pH.Create(Toppings);
+
+                return RedirectToPage("MenuTable");
+            }
+            return Page();
+        }
+    }
+}
