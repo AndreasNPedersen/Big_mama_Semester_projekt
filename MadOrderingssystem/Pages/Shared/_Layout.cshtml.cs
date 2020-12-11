323a676d563f94cd61987a1569c878bbe2261794
Mikkel Hartmann Jensen <mikk4472@edu.easj.dk>
2020-12-11 14:54:58 +0100

    Try catch til kontrolpanels dele

--- MadOrderingssystem/Pages/Shared/_Layout.cshtml.cs
+++ MadOrderingssystem/Pages/Shared/_Layout.cshtml.cs
@@ -2,15 +2,25 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MadOrderingssystem.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace MadOrderingssystem.Pages.Shared
 {
     public class _LayoutModel : PageModel
     {
+        public Customer CustomerSession { get; set; }
+
         public void OnGet()
         {
+            try
+            {
+                CustomerSession = JsonConvert.DeserializeObject<Customer>(HttpContext.Session.GetString("user"));
+            }
+            catch (ArgumentNullException ex) { }
         }
     }
 }
